c5dae8da831af5cebe286474645bd9760772fcc6
Owen Palmer <owen@lunatechnw.com>
2020-12-30 18:51:07 +0000

    database.php changed somehow, about to reset the site due to malware

--- database.php
+++ database.php
@@ -3,12 +3,14 @@ include('/var/www/owenpalmer.com/htdocs/wp-load.php');
 global $wpdb;
 $nickname = $_POST['nickname'];
 $score = $_POST['score'];
+$death = $_POST['death'];
 
 $wpdb->insert(
     'wp_snake',
     array(
         'name' => $nickname,
-        'apples' => $score
+        'apples' => $score,
+        'death' => $death
     )
 );
 
